217db67acc468523de0382088b6c419f035583c8
Dat Tran <dattran306@gmail.com>
2021-11-30 01:44:58 +0700

    lab05 part 4

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -22,7 +22,7 @@ class Main extends Component {
       }
   }
 
-
+  
 
   render() {
     const HomePage = () => {
@@ -34,12 +34,23 @@ class Main extends Component {
         />
       )
     }
+
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail
+          dish={this.state.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((commemt)=> commemt.id === parseInt(match.params.dishId,10))[0]}
+        />   
+      )
+    }
+
     return (
       <div>
         <Header />
           <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Route path="/menu/:dishId" component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />
             <Redirect to="/home" />
           </Switch>
